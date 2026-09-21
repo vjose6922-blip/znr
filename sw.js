63975d4b25db3d021192bfc8a302fbabe918e2de
vjose6922-blip <vjose6922@gmail.com>
2026-09-21 06:12:35 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME    = 'zr-cache-v91';
-const DYNAMIC_CACHE = 'zr-dynamic-v21';
+const CACHE_NAME    = 'zr-cache-v92';
+const DYNAMIC_CACHE = 'zr-dynamic-v22';
 const OFFLINE_URL   = '/znr/offline.html';
 
 const STATIC_ASSETS = [
